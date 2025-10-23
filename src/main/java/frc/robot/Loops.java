6dc8ea8d5b71688ef47a0a9104dfe3bd5391fedc
tfluck117 <tfluck053@gmail.com>
2025-10-22 20:31:14 -0500

    starting the first exercise in the loops file

--- src/main/java/frc/robot/Loops.java
+++ src/main/java/frc/robot/Loops.java
@@ -1,4 +1,5 @@
 package frc.robot;
+import java.util.Scanner;
 
 public class Loops {
     /* Exercise 1
@@ -12,4 +13,15 @@ public class Loops {
      * and make sure it is present. The for loop should act to emulate the exponent function
      * same as before
      */
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the base number: ");
+        double base = s.nextDouble();
+        System.out.println("You entered "+base+" as your base");
+        System.out.println("Enter the exponent number: ");
+        double exponent = s.nextDouble();
+        System.out.println("You entered "+exponent+" as your exponent");
+
+        
+    }
 }
