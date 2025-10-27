da8662b166d53e34e663609619f15a5ab046ea49
Umar K <umarkhanayatampa@gmail.com>
2025-10-27 10:24:39 -0500

    Car/Vehicle Exercises
    preparing for today and maybe wednesday lessons. going over functions, static keyword, inheritance, and constructors before going to frc

---
+++ src/main/java/frc/robot/Car.java
@@ -0,0 +1,16 @@
+package frc.robot;
+
+public class Car {
+    /*
+     * Exercise 2:
+     * This class should be a child class of the vehicle class you made previously.
+     * You should add some some limits to this to try and simulate it being a car.
+     * For example, you should not exceed a certain amount of gas held in your car
+     * or limited the amount of passengers, etc. You guys have freedom to do what you
+     * like with this class but make sure to have a constructor, functions, and variables.
+     * Add a pvsm and print things out. no static variables and play around with inheritance.
+     * The goal of this exercise is both to get some creativity going and learn inheritance.
+     * Feel free to use the internet for things btw but for your sake don't use AI or you
+     * won't learn much at all
+     */
+}
