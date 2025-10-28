f669605f07b08858454aa75b1d7f1470caf9f38b
Umar K <umarkhanayatampa@gmail.com>
2025-10-28 13:06:45 -0500

    vehicle solutions

--- src/main/java/frc/robot/Vehicle.java
+++ src/main/java/frc/robot/Vehicle.java
@@ -17,11 +17,29 @@ public class Vehicle {
      * using objects since your variables are no longer static
      */
 
+    private double gallons;
+    private double mileage;
+    private int passengers;
+    private double xPosition;
+    private double yPosition;
+    private String direction;
+    private String brand;
+    private boolean parked;
+
     /*
     * Constructor
     * Create a constructor that initializes all your variables using parameters
     */
-    public Vehicle() {}
+    public Vehicle(int gallons, int mileage, int passengers, double xPosition, double yPosition, String direction, String brand, boolean parked) {
+        this.gallons = gallons;
+        this.mileage = mileage;
+        this.passengers = passengers;
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+        this.direction = direction;
+        this.brand = brand;
+        this.parked = parked;
+    }
 
 
 
@@ -31,11 +49,39 @@ public class Vehicle {
      * decide if you need parameters and how this will work
     */
 
+    public void turnRight() {
+        if(direction.equals("south")) {
+            direction = "west";
+        } else if(direction.equals("west")) {
+            direction = "north";
+        } else if(direction.equals("north")) {
+            direction = "east";
+        } else if(direction.equals("east")) {
+            direction = "south";
+        } else {
+            System.out.println("Invalid Direction, please set direction");
+        }
+    }
+
 
     /* 
      * turnLeft 
      * same thing as above function but opposite direction
     */
+
+    public void turnLeft() {
+        if(direction.equals("south")) {
+            direction = "east";
+        } else if(direction.equals("west")) {
+            direction = "south";
+        } else if(direction.equals("north")) {
+            direction = "west";
+        } else if(direction.equals("east")) {
+            direction = "north";
+        } else {
+            System.out.println("Invalid Direction, please set direction");
+        }
+    }
 
  
     /*
@@ -47,11 +93,34 @@ public class Vehicle {
      * you must send an alert if you are out of gas or run out of gas during the movement
     */
 
+    public void move(int miles) {
+        if(!parked && (miles * mileage) > gallons) {
+            if(direction.equals("south")) {
+                yPosition -= miles;
+            } else if(direction.equals("west")) {
+                xPosition -= miles;
+            } else if(direction.equals("north")) {
+                yPosition += miles;
+            } else if(direction.equals("east")) {
+                xPosition += miles;
+            } else {
+                System.out.println("Invalid Direction, please set direction");
+            }
+        } else if(parked) {
+            System.out.println("You are parked so you cannot move");
+        } else {
+            System.out.println("Invalid Direction, please set direction");
+        }
+    }
 
     /*
      * getXCoordinate
      * return X coordinate
     */
+
+    public double getXCoordinate() {
+        return xPosition;
+    }
 
 
     /*
@@ -59,11 +128,19 @@ public class Vehicle {
      * return Y coordinate
     */
 
+    public double getYCoordinate() {
+        return yPosition;
+    }
+
         
     /*
      * getRange
      * return the amount of miles you can go without refilling gas
     */
+
+    public double getRange() {
+        return gallons / mileage;
+    }
 
         
     /*
@@ -71,11 +148,19 @@ public class Vehicle {
      * refill the gas by certain amount of gallons
     */
 
+    public void refillGas(double gallons) {
+        this.gallons = this.gallons + gallons;
+    }
+
 
     /*
      * getBrand
      * return the brand
     */
+
+    public String getBrand() {
+        return brand;
+    }
 
 
     /*
@@ -83,21 +168,35 @@ public class Vehicle {
      * return amount of passengers
     */
 
+    public int getPassengers() {
+        return passengers;
+    }
 
     /*
      * setPassengers
      * set the amount of passengers
     */
 
+    public void setPassengers(int passengers) {
+        this.passengers = passengers;
+    }
 
     /*
      * isParked
      * return if vehicle is parked or not
      */
 
+    public boolean isParked() {
+        return parked;
+    }
+
     
     /*
      * setParked
      * set whether parked or not
      */
+
+    public void setParked(boolean parked) {
+        this.parked = parked;
+    }
 }
